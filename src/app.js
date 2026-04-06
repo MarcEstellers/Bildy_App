fdd12ba1913264a1c37ab35a6f60ebdcd888b478
MarcEstellers <marc.estellers@gmail.com>
2026-04-06 19:57:30 +0200

    Handle JWT , auth middleware  y routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(express.json());//uso express
+app.use(helmet());//uso helmet 
 
 export default app;
